6bca30f4c173438923cf9789ff0da44b524c13e7
Xavier Chapron <xavier.chapron@ledger.fr>
2023-08-03 18:16:16 +0200

    tests: speculos: Fasten a bit some tests

--- tests/speculos/u2f/test_register_cmd.py
+++ tests/speculos/u2f/test_register_cmd.py
@@ -82,7 +82,7 @@ def test_register_duplicate(client):
 
 
 def test_register_multiple_ok(client):
-    for i in range(10):
+    for i in range(5):
         challenge = generate_random_bytes(32)
         app_param = generate_random_bytes(32)
 
